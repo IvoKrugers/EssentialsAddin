8eaf5fa3a4447552eda0026ac89d3583f217611e
Ivo Krugers <i.krugers@xablu.com>
2020-04-09 10:29:57 +0200

    Update Microsoft.Bcl.Build to 1.0.21

--- EssentialsAddin/Constants.cs
+++ EssentialsAddin/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string Version = "1.7.0";
+        public const string Version = "1.7.1";
         public const string SolutionFilterPadId = "EssentialsAddin.SolutionFilterPad";
         public const string OutputFilterPadId = "EssentialsAddin.OutputFilterPad";
     }
