4ededcef05418bc8408837ecff4caf7329a07ab7
Ivo Krugers <i.krugers@xablu.com>
2020-04-16 10:27:30 +0200

    update repo and readme

--- mpack/updateRepo.sh
+++ mpack/updateRepo.sh
@@ -9,10 +9,13 @@ cd ${PROJECTFOLDER}
 
 pwd
 
-rm *.mpack
-mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack ../EssentialsAddin/bin/Release/EssentialsAddin.dll
+cd EssentialsAddin
 
+rm *.mpack
+mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack ../../EssentialsAddin/bin/Release/EssentialsAddin.dll
+
+cd ..
 
 /Applications/Visual\ Studio.app/Contents/MacOS/vstool setup rep-build .
-#rm index.html
-#xml ed -i "/Repository" -t text -n "Name" -v "Extensions by Arthur Demanuele" main.mrep
+rm index.html
+#xml ed -i "/Repository" -t text -n "Name" -v "Essentials Addin by Ivo Krugers" main.mrep
