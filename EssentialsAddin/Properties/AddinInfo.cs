885dc4113e199319aefb0f2e930e0505820a8d6a
Ivo Krugers <i.krugers@xablu.com>
2020-04-16 13:10:43 +0200

    update addin properties

--- EssentialsAddin/Properties/AddinInfo.cs
+++ EssentialsAddin/Properties/AddinInfo.cs
@@ -3,21 +3,23 @@ using Mono.Addins;
 using Mono.Addins.Description;
 
 [assembly: Addin(
-	"Essentials Addin",
+	"EssentialsAddin",
 	Namespace = "EssentialsAddin",
 	Version = EssentialsAddin.Constants.Version
 )]
 
-[assembly: AddinName("EssentialsAddin")]
+[assembly: AddinName("Essentials Addin")]
 [assembly: AddinCategory("IDE extensions")]
-[assembly: AddinDescription("Essentials Addin provides:\n"+
+[assembly: AddinDescription("Essentials Addin provides several pads and other functionalities. \n"+
+    "Funtionality summary: \n"+
     " - Solution tree filtering (Pad)\n" +
     " - One click to open file functionality. \n" +
     " - Filter project to Expand\n"+
     " - Filter Application Output in a new Pad\n" +
     "\n" +
-    "The pads can be opened from the Tools -> Essentials")]
+    "The pads can be opened from the View-Pads or Tools menu.\n\nby Ivo Krugers")]
 [assembly: AddinAuthor("Ivo Krugers")]
+[assembly: AddinUrl("https://github.com/IvoKrugers/EssentialsAddin")]
 
 [assembly: AddinDependency("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
 [assembly: AddinDependency("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
