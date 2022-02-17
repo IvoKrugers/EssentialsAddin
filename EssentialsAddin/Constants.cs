c0d3a98f20c0e733d5c7cb48985290c0969afa05
Ivo Krugers <i.krugers@xablu.com>
2022-02-17 17:14:08 +0100

    pack 2.1.0

--- EssentialsAddin/Constants.cs
+++ EssentialsAddin/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string Version = "2.0.0";
+        public const string Version = "2.1.0";
         public const string SolutionFilterPadId = "EssentialsAddin.SolutionFilterPad";
         public const string OutputFilterPadId = "EssentialsAddin.OutputFilterPad";
     }
