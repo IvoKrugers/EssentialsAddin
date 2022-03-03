e4adb5ad18c1fa72849eb3c717a31c9f1fbacd82
Ivo Krugers <i.krugers@xablu.com>
2022-03-03 22:15:32 +0100

    update refs

--- EssentialsAddin/Constants.cs
+++ EssentialsAddin/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string Version = "2.2.2";
+        public const string Version = "2.2.3";
         public const string SolutionFilterPadId = "EssentialsAddin.SolutionFilterPad";
         public const string OutputFilterPadId = "EssentialsAddin.OutputFilterPad";
     }
