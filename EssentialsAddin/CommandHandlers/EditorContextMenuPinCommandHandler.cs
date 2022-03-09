dbab982db8333d1098c0b2196776bb3857296d15
Ivo Krugers <i.krugers@xablu.com>
2022-03-09 11:13:11 +0100

    add context menu to editor to pin/unpin that document

---
+++ EssentialsAddin/CommandHandlers/EditorContextMenuPinCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using EssentialsAddin.SolutionFilter;
+using MonoDevelop.Components.Commands;
+
+namespace EssentialsAddin.CommandHandlers
+{
+    public class EditorContextMenuPinCommandHandler: FileNodeContextMenuPinCommandHandler
+    {
+    }
+}
